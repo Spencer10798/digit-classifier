6f1b5809537ff386a9d68bde191be1314128d76b
Spencer10798 <maghrouri@hotmail.com>
2023-03-22 11:32:30 -0400

    nn architecture

--- main.py
+++ main.py
@@ -27,3 +27,12 @@ def load_data(train_csv, test_csv):
         return train_data, test_data
 
 
+class MNISTClasifier(nn.module):
+    def __init__(self):
+           self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+           self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+           self.fc1 = nn.Linear(64 * 7 * 7, 128)
+           self.fc2 = nn.Linear(128, 10)
+           self.pool = nn.MaxPool2d(2,2)
+           self.relu = nn.ReLU()
+
