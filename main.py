df8ad16bfcb91c2a4b6eac15c15c8f05de729643
Spencer10798 <maghrouri@hotmail.com>
2023-03-22 11:34:54 -0400

    forward func

--- main.py
+++ main.py
@@ -36,3 +36,11 @@ class MNISTClasifier(nn.module):
            self.pool = nn.MaxPool2d(2,2)
            self.relu = nn.ReLU()
 
+    def forward(self, x):
+           x = self.pool(self.relu(self.conv1(x)))
+           x = self.pool(self.relu(self.conv2(x)))
+           x = x.view(-1, 64 * 7 * 7)
+           x = self.relu(self.fc1(x))
+           x = self.fc2(x)
+           return x
+
