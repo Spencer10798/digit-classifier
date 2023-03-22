9dda3673454b0f34cc76afc7ab3547802da7eb9c
Spencer10798 <maghrouri@hotmail.com>
2023-03-22 11:57:15 -0400

    eval

--- main.py
+++ main.py
@@ -54,3 +54,16 @@ def train_model(model, dataloader, criterion, optimizer, device):
             optimizer.step()
             running_loss += loss.item()
     return running_loss / len(dataloader)
+
+def evaluate_model(model, dataloader, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in dataloder:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return correct / total
